acee8c1ac530e34bb39b6a8ec6399adfa9a07b6c
Maarten Breddels <maartenbreddels@gmail.com>
2018-08-28 14:09:47 -0400

    Initial commit, was missing, fixes #4

---
+++ flask_ipywidgets/static/main.js
@@ -0,0 +1,30 @@
+
+// Initialize requirejs (for dynamically loading widgets)
+// and render widgets on page.
+
+requirejs.config({
+    baseUrl: '/static/dist/'
+})
+
+require(['libwidgets'], function(lib) {
+    var BASEURL = window.location.href
+
+    var WSURL;
+    if (window.location.protocol.startsWith('https')) {
+        WSURL = 'wss://' + window.location.host
+    }
+    else {
+        WSURL = 'ws://' + window.location.host
+    }
+
+    if (document.readyState === 'complete') {
+        lib.renderWidgets(BASEURL, WSURL, lib.requireLoader);
+    } else {
+        window.addEventListener(
+            'load',
+            function() {
+                lib.renderWidgets(BASEURL, WSURL, lib.requireLoader);
+            }
+        );
+    }
+});
